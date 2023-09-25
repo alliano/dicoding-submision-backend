c50d229542e6183d37132586b2112349fd631be6
alliano <allianoanonymous@gmail.com>
2023-09-25 11:48:15 +0900

    add functionality to delete book

--- src/handlers.js
+++ src/handlers.js
@@ -100,4 +100,20 @@ const updateBookById = (request, h) => {
      }).code(200);
 }
 
-module.exports = { addBookHandler, getAllBooksHandler, getBookById, updateBookById }
+const deleteBookById = (request, h) => {
+    const { id } = request.params;
+    const index = books.findIndex(book => book.id === id);
+    if (index === -1) {
+        return h.response({
+            status: 'fail',
+            message: 'Buku gagal dihapus. Id tidak ditemukan'
+        }).code(404);
+    }
+    books.splice(index, 1);
+    return h.response({
+        status: 'success',
+        message: 'Buku berhasil dihapus'
+    }).code(200);
+}
+
+module.exports = { addBookHandler, getAllBooksHandler, getBookById, updateBookById, deleteBookById }
